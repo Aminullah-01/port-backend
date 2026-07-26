1415895750ec60406addd313273fc062ab017692
Aminu Gambo Abubakar <aminugamboabubakar33@gmail.com.com>
2026-07-26 17:57:17 +0100

    created admin user

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'your-email@example.com'],
+            [
+                'name' => 'Aminullah',
+                'password' => Hash::make('@Abubakar01'),
+            ]
+        );
+    }
+}
